4eeca2a07c160fd732a642aebaf180c3ac20908c
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2017-03-04 08:08:44 +0100

    start home/add lost property design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,23 +6,23 @@
       <section id="pageTagline">
           <div class="thePageTagLine">
             {{strstr(Auth::user()->name . ' ', ' ', true )}} 's Dashboard
-          </div>
-      </section>
-<div class="container-fluid">
-    <div class="row" style="">
-        <div class="col-xl-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <div class="panel-heading-text"><img src="img/cog.svg" width="35" height="35"/>Quick Actions</div>
-                </div>
+        </div>
+    </section>
+    <div class="container-fluid">
+        <div class="row" style="">
+            <div class="col-xl-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="panel-heading-text"><img src="img/cog.svg" width="35" height="35"/>Quick Actions</div>
+                    </div>
 
-                <div class="panel-body" class="openSans">
-                    <div class="panel-body-text">
-                        View my approved items
+                    <div class="panel-body" class="openSans">
+                        <div class="panel-body-text">
+                            View my approved items
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
